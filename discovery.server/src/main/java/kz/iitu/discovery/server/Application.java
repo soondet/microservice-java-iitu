f3db45a196c48b4ff93889632e01e833490d5712
soondet <soondet@mail.ru>
2020-10-04 18:34:17 +0200

    New discovery service

--- discovery.server/src/main/java/kz/iitu/discovery/server/Application.java
+++ discovery.server/src/main/java/kz/iitu/discovery/server/Application.java
@@ -1,11 +1,11 @@
-package kz.iitu.location.management;
+package kz.iitu.discovery.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableEurekaSer
 public class Application {
 
 	public static void main(String[] args) {
